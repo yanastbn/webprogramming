6600264e7abe84b429a199410b14b9a2f51f9234
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2024-10-28 20:23:09 +0800

    Update sample_db.sql

--- sample_db.sql
+++ sample_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2024 at 07:09 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Oct 28, 2024 at 01:22 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,6 @@ USE `sample_db`;
 -- Table structure for table `account`
 --
 
-DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
   `id` int(11) NOT NULL,
   `first_name` varchar(255) NOT NULL,
@@ -37,8 +36,8 @@ CREATE TABLE `account` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `role` varchar(100) NOT NULL,
-  `is_staff` smallint(6) NOT NULL DEFAULT 1,
-  `is_admin` smallint(6) NOT NULL DEFAULT 0
+  `is_staff` tinyint(4) NOT NULL DEFAULT 1,
+  `is_admin` tinyint(4) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -46,9 +45,8 @@ CREATE TABLE `account` (
 --
 
 INSERT INTO `account` (`id`, `first_name`, `last_name`, `username`, `password`, `role`, `is_staff`, `is_admin`) VALUES
-(2, 'Dean', 'Billedo', 'admin', '$2y$10$NWHfb3j672MK8UcUdrbRpOPfItFBf4ibZy/JTmjEJEtSy8JDU4PU6', 'admin', 1, 1),
-(3, 'Ald', 'Suarez', 'staff', '$2y$10$1IgUt8SV3Nkd8Uwel53ZPu1o4PXgGcu1YX64r1x7lsiVNYyPiDdAa', 'staff', 1, 0),
-(4, 'Natsu', 'Dragneel', 'customer', '$2y$10$NTHKgW6ViyGcrr/rnnDf0ueT1p9jtfwQ1tdgSxg5SqLJliV2iJC2G', 'customer', 0, 0);
+(2, 'Admin', 'Admin', 'admin', '$2y$10$kIsio70mGMOC52N1kGgSC.4ESyfmdXPojSnxP.8IDnB8c1Z4brw7G', 'admin', 1, 1),
+(3, 'Staff', '01', 'staff', '$2y$10$105HdzfebWx8R8q8x2WvZeZlfF6ASKNH2zKFGhh2mpaWvFjYxKdte', 'staff', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -56,7 +54,6 @@ INSERT INTO `account` (`id`, `first_name`, `last_name`, `username`, `password`, 
 -- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `category`;
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
@@ -67,8 +64,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
-(1, 'Gadget'),
-(2, 'Toys');
+(1, 'Pizza'),
+(2, 'Refreshment');
 
 -- --------------------------------------------------------
 
@@ -76,7 +73,6 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- Table structure for table `product`
 --
 
-DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `code` varchar(255) NOT NULL,
@@ -92,8 +88,30 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `code`, `name`, `category_id`, `price`, `created_at`, `updated_at`) VALUES
-(32, 'A001', 'Laptop Dell', 1, '129.00', '2024-09-11 00:08:45', '2024-09-17 11:21:39'),
-(33, 'A002', 'Cellphone 10', 1, '89.00', '2024-09-11 00:08:57', '2024-09-17 11:21:29');
+(32, 'A001', 'Spicy Pizza', 1, '299.00', '2024-09-11 00:08:45', '2024-10-28 12:17:42'),
+(33, 'A002', 'Hawaiian', 1, '199.00', '2024-09-11 00:08:57', '2024-10-28 12:18:13'),
+(41, 'A003', 'Veggie Pizza', 1, '199.00', '2024-10-28 12:19:13', '2024-10-28 12:19:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_image`
+--
+
+CREATE TABLE `product_image` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `image_role` varchar(100) NOT NULL,
+  `uploaded_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_image`
+--
+
+INSERT INTO `product_image` (`id`, `product_id`, `file_path`, `image_role`, `uploaded_at`) VALUES
+(2, 41, '../uploads/671f81415a4873.jpg', 'main', '2024-10-28 12:19:13');
 
 -- --------------------------------------------------------
 
@@ -101,7 +119,6 @@ INSERT INTO `product` (`id`, `code`, `name`, `category_id`, `price`, `created_at
 -- Table structure for table `stocks`
 --
 
-DROP TABLE IF EXISTS `stocks`;
 CREATE TABLE `stocks` (
   `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
@@ -135,7 +152,7 @@ INSERT INTO `stocks` (`id`, `product_id`, `quantity`, `status`, `reason`, `creat
 --
 ALTER TABLE `account`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `ausername` (`username`);
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- Indexes for table `category`
@@ -152,10 +169,18 @@ ALTER TABLE `product`
   ADD KEY `category_id` (`category_id`);
 
 --
+-- Indexes for table `product_image`
+--
+ALTER TABLE `product_image`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_product_image` (`product_id`);
+
+--
 -- Indexes for table `stocks`
 --
 ALTER TABLE `stocks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_products` (`product_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -165,7 +190,7 @@ ALTER TABLE `stocks`
 -- AUTO_INCREMENT for table `account`
 --
 ALTER TABLE `account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `category`
@@ -177,7 +202,13 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `product_image`
+--
+ALTER TABLE `product_image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `stocks`
@@ -194,6 +225,18 @@ ALTER TABLE `stocks`
 --
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+
+--
+-- Constraints for table `product_image`
+--
+ALTER TABLE `product_image`
+  ADD CONSTRAINT `fk_product_image` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Constraints for table `stocks`
+--
+ALTER TABLE `stocks`
+  ADD CONSTRAINT `fk_products` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
